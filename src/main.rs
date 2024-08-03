6817b267bff443bad17b5a6241f309e096963307
Mohit Patel <m265pate@uwaterloo.ca>
2024-08-03 16:15:41 -0400

    wonderful code - are the arguments directories??

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,33 @@ use clap::Parser;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-    /// The path to Dropbox files to move
-    path: std::path::PathBuf,
+    // The path to move data from
+    from_path: std::path::PathBuf,
+    // The path to move data to
+    to_path: std::path::PathBuf
 }
 
 fn main() {
     let args = Cli::parse();
+    println!("from_path: {:?}, to_path: {:?}", args.from_path, args.to_path);
+    let from_is_dir = args.from_path.is_dir();
+    let to_is_dir = args.to_path.is_dir();
+    if !from_is_dir || !to_is_dir {
+        
+        if !from_is_dir {
+            println!("{:?} is not a valid directory", args.from_path);
+        }
+        if !to_is_dir {
+            println!("{:?} is not a valid directory", args.to_path);
+        }
+        panic!("Failed to run the program - both paths are not directories.")
 
-    println!("path: {:?}", args.path)
+    } else {
+        println!("yay we have 2 dirs!")
+    }
+    // let copy_result = std::fs::copy(args.from_path, args.to_path);
+    // match copy_result {
+    //     Ok(content) => { println!("copy success? {}", content);} 
+    //     Err(error) => { println!("ahhhhhhhhhhh couldn't copy {}", error)}
+    // }
 }
